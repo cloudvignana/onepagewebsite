f4d96c6aa35e8c7cc561ae8f830decbb3443abef
Sairam L <sairamkrishna1235@gmail.com>
2022-11-02 11:47:16 +0530

    Best simple nodejs website for container learning

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const HOST = '0.0.0.0';
+
+app.use(express.json());
+app.use(express.static("express"));
+// default URL for website
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/express/index.html'));
+  });
+const server = http.createServer(app);
+
+server.listen(port);
+console.log(`Running on http://${HOST}:${port}`);
