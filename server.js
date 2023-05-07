30469fa67ddf68f43f63b08895f932da93f00242
Sairam L <sairamkrishna1235@gmail.com>
2023-05-07 17:03:31 +0530

    fix sec issue #6

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const HOST = '0.0.0.0';
 
 app.use(express.json());
 app.use(express.static("express"));
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 // default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
